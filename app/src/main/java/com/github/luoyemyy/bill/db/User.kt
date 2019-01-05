89cec7581ea4fa3dd77cddc139973cbb8c01e407
luoyemyy <luoyemyy@sna.com>
2019-01-05 18:24:03 +0800

    update

--- app/src/main/java/com/github/luoyemyy/bill/db/User.kt
+++ app/src/main/java/com/github/luoyemyy/bill/db/User.kt
@@ -5,16 +5,24 @@ import androidx.room.*
 
 
 @Entity
-data class User(@PrimaryKey(autoGenerate = true) var id: Long = 0,
-                var nickname: String? = null,
-                var isDefault: Int = 0
+data class User(
+    @PrimaryKey(autoGenerate = true) var id: Long = 0,
+    var nickname: String? = null,
+    var intro: String? = null,
+    var isDefault: Int = 0
 )
 
 @Dao
 interface UserDao {
 
     @Insert
-    fun add(user: User)
+    fun add(user: User): Long
+
+    @Update
+    fun update(users: List<User>): Long
+
+    @Query("select * from user")
+    fun getAll(): LiveData<List<User>>
 
     @Query("select * from user where isDefault = 1 limit 1")
     fun getDefault(): LiveData<User>
---
+++ app/src/main/java/com/github/luoyemyy/bill/util/UserInfo.kt
@@ -0,0 +1,9 @@
+package com.github.luoyemyy.bill.util
+
+import android.content.Context
+
+object UserInfo {
+
+    fun getUserId(context: Context):Long = 0L
+
+}
